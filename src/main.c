4bdf294d313c3ae5c57f2f09bc280a8626ee60e3
M1lkZ <nickolasmarwin@gmail.com>
2023-02-02 09:35:31 +0300

    lab done

---
+++ src/main.c
@@ -0,0 +1,186 @@
+#include "mem.h"
+#include "mem_internals.h"
+#include "util.h"
+#include <stdint.h>
+
+#define HEAP_INIT_SIZE 4096
+
+static struct block_header *block_get_header(void *contents) {
+    return (struct block_header *) (((uint8_t *) contents) - offsetof(struct block_header, contents));
+}
+
+void *heap;
+
+void test_0_heap_creation() {
+    heap = heap_init(HEAP_INIT_SIZE);
+    if (heap == NULL) {
+        munmap(heap, HEAP_INIT_SIZE);
+        err("Failed test 0: heap initialization didn't happen");
+    }
+    printf("Test 0 succeed \n");
+    printf("Heap final: \n");
+    debug_heap(stdout, heap);
+    munmap(heap, HEAP_INIT_SIZE);
+}
+
+void test_1_malloc() {
+    heap = heap_init(HEAP_INIT_SIZE);
+    if (heap == NULL) {
+        munmap(heap, HEAP_INIT_SIZE);
+        err("Failed test 1: Heap initialization didn't happen");
+    }
+    void *alloc = _malloc(2048);
+    if (alloc == NULL) {
+        debug_heap(stderr, heap);
+        err("Failed test 1: Couldn't allocate memory");
+    }
+    puts("Heap before freeing \n");
+    debug_heap(stdout, heap);
+    _free(alloc);
+    puts("Heap after freeing \n");
+    debug_heap(stdout, heap);
+    printf("Test 1 succeed");
+    munmap(heap, HEAP_INIT_SIZE);
+}
+
+void test_2_free_one_region() {
+    heap = heap_init(HEAP_INIT_SIZE);
+    if (heap == NULL) {
+        munmap(heap, HEAP_INIT_SIZE);
+        err("Failed test 2: Heap initialization didn't happen");
+    }
+    void *alc0 = _malloc(1024);
+    void *alc1 = _malloc(1024);
+    if (alc0 == NULL || alc1 == NULL) {
+        printf("Heap on fail \n");
+        debug_heap(stderr, heap);
+        munmap(heap, HEAP_INIT_SIZE);
+        err("Failed test 2: Couldn't allocate memory");
+    }
+    printf("Heap before freeing \n");
+    debug_heap(stdout, heap);
+    _free(alc0);
+    if (alc1 == NULL) {
+        debug_heap(stderr, heap);
+        err("Failed test 2: Freeing one region damages the other");
+    }
+    _free(alc1);
+    printf("Heap after freeing \n");
+    debug_heap(stdout, heap);
+    printf("Test 2 succeed");
+    munmap(heap, HEAP_INIT_SIZE);
+}
+
+void test_3_mem_end() {
+    heap = heap_init(HEAP_INIT_SIZE);
+    if (heap == NULL) {
+        munmap(heap, HEAP_INIT_SIZE);
+        err("Failed test 3: Heap initialization didn't happen");
+    }
+    void *allocated0 = _malloc(4096);
+    void *allocated1 = _malloc(4096);
+    struct block_header *header0 = block_get_header(allocated0);
+    struct block_header *header1 = block_get_header(allocated1);
+    if (header0->next != header1) {
+        munmap(heap, size_from_capacity((block_capacity){.bytes = 4096}).bytes);
+        err("Failed test 3: Headers are not linked");
+    }
+    _free(allocated0);
+    _free(allocated1);
+    printf("Test 3 succeed \n");
+    munmap(heap, HEAP_INIT_SIZE);
+}
+
+void test_4_several_blocks() {
+    heap = heap_init(HEAP_INIT_SIZE);
+    if (heap == NULL) {
+        munmap(heap, HEAP_INIT_SIZE);
+        err("Failed test 4: Heap initialization didn't happen");
+    }
+    printf("Heap before allocating \n");
+    debug_heap(stdout, heap);
+    void *allocated0 = _malloc(1024*4);
+    void *allocated1 = _malloc(1024*16);
+    printf("Heap after allocating \n");
+    debug_heap(stdout, heap);
+    struct block_header *block_0 = block_get_header(allocated0);
+    struct block_header *block_1 = block_get_header(allocated1);
+    if (block_0 == NULL || block_0->next != block_1) {
+        munmap(heap, HEAP_INIT_SIZE);
+        err("Failed test 4: Blocks damaged or not linked");
+    }
+
+    printf("Test 4 succeed \n");
+    _free(allocated0);
+    _free(allocated1);
+    munmap(heap, HEAP_INIT_SIZE);
+}
+
+void test_5_another_heap() {
+    heap = heap_init(HEAP_INIT_SIZE);
+    if (heap == NULL) {
+        munmap(heap, HEAP_INIT_SIZE);
+        err("Failed test 5: Heap initialization didn't happen");
+    }
+    void *allocated0 = _malloc(1024*10);
+    if (allocated0 == NULL){
+        munmap(heap, HEAP_INIT_SIZE);
+        err("Failed test 5: First allocating gone wrong");
+    }
+    struct block_header *header0 = block_get_header(allocated0);
+    if (header0 == NULL){
+        munmap(heap, HEAP_INIT_SIZE);
+        err("Failed test 5: Couldn't get first header");
+    }
+    struct block_header *next_block = header0->next;
+    printf("Heap after allocating \n");
+    debug_heap(stdout, header0);
+    void *reg = mmap(next_block->contents + next_block->capacity.bytes, 256, PROT_READ | PROT_WRITE, MAP_PRIVATE | 0x20, -1, 0);
+    if (reg == NULL) {
+        munmap(heap, HEAP_INIT_SIZE);
+        err("Failed test 5: Couldn't map region");
+    }
+    printf("Heap after mapping \n");
+    debug_heap(stdout, header0);
+    void *allocated1 = _malloc(1024*4);
+    if (allocated1 == NULL){
+        munmap(heap, HEAP_INIT_SIZE);
+        err("Failed test 5: Second allocating gone wrong");
+    }
+    struct block_header *header1 = block_get_header(allocated1);
+    if (header1 == NULL){
+        munmap(heap, HEAP_INIT_SIZE);
+        err("Failed test 5: Couldn't get second header");
+    }
+    if (!(header0->is_free || header1->is_free)) {
+        _free(allocated0);
+        _free(allocated1);
+    }
+    debug_heap(stdout, header0);
+    printf("Test 5 succeed \n");
+    munmap(heap, HEAP_INIT_SIZE);
+}
+
+
+int main() {
+    printf("Running test 0 \n");
+    test_0_heap_creation();
+    printf("===============\n");
+    printf("Running test 1 \n");
+    test_1_malloc();
+    printf("===============\n");
+    printf("Running test 2 \n");
+    test_2_free_one_region();
+    printf("===============\n");
+    printf("Running test 3 \n");
+    test_3_mem_end();
+    printf("===============\n");
+    printf("Running test 4 \n");
+    test_4_several_blocks();
+    printf("===============\n");
+    printf("Running test 5 \n");
+    test_5_another_heap();
+    printf("===============\n");
+    return 0;
+}
+
--- src/mem.c
+++ src/mem.c
@@ -39,8 +39,20 @@ static void* map_pages(void const* addr, size_t length, int additional_flags) {
 
 /*  аллоцировать регион памяти и инициализировать его блоком */
 static struct region alloc_region  ( void const * addr, size_t query ) {
-  /*  ??? */
+    size_t act_size = region_actual_size(size_from_capacity((block_capacity) {.bytes = query}).bytes);
+    void* area = map_pages(addr, act_size, MAP_FIXED_NOREPLACE);
+    if (area == MAP_FAILED) {
+        area = map_pages(addr, act_size, 0);
+    }
+    if (area != MAP_FAILED) {
+        struct region allocated_region = (struct region){.addr = area, .size = act_size, .extends = (area == addr)};
+        block_init(area, (block_size){.bytes = act_size}, NULL);
+        return allocated_region;
+    }
+    return REGION_INVALID;
 }
+
+
 
 static void* block_after( struct block_header const* block )         ;
 
@@ -60,7 +72,15 @@ static bool block_splittable( struct block_header* restrict block, size_t query)
 }
 
 static bool split_if_too_big( struct block_header* block, size_t query ) {
-  /*  ??? */
+  if (block_splittable(block, query)){
+      void* query_offset = block->contents + query;
+      block_size new_block_size = (block_size){.bytes=(block->capacity.bytes-query)};
+      block_init(query_offset, new_block_size, block->next);
+      block->capacity.bytes = query;
+      block->next = query_offset;
+      return true;
+  }
+  return false;
 }
 
 
@@ -80,7 +100,11 @@ static bool mergeable(struct block_header const* restrict fst, struct block_head
 }
 
 static bool try_merge_with_next( struct block_header* block ) {
-  /*  ??? */
+  if(!mergeable(block, block->next) || block->next == NULL) return false;
+  struct block_header* nxt = block->next;
+  block->capacity.bytes += size_from_capacity(nxt->capacity).bytes;
+  block->next = nxt->next;
+  return true;
 }
 
 
@@ -92,27 +116,62 @@ struct block_search_result {
 };
 
 
-static struct block_search_result find_good_or_last  ( struct block_header* restrict block, size_t sz )    {
-  /*??? */
+static struct block_search_result find_good_or_last  ( struct block_header* restrict block, size_t sz ){
+    if (block == NULL) return (struct block_search_result) {.type = BSR_CORRUPTED, .block = NULL};
+    bool merged = true;
+    while (block != NULL){
+        do {
+            merged = try_merge_with_next(block);
+        } while(merged);
+        if (block->is_free && block_is_big_enough(sz, block)){
+            return (struct block_search_result) {.type = BSR_FOUND_GOOD_BLOCK, .block = block};
+        }
+        if (block->next == NULL) {
+            return (struct block_search_result) {.type = BSR_REACHED_END_NOT_FOUND, .block = block};
+        }
+        block = block->next;
+    }
+    return (struct block_search_result) {.type = BSR_REACHED_END_NOT_FOUND, .block = NULL};
 }
 
 /*  Попробовать выделить память в куче начиная с блока `block` не пытаясь расширить кучу
  Можно переиспользовать как только кучу расширили. */
 static struct block_search_result try_memalloc_existing ( size_t query, struct block_header* block ) {
-  
+  struct block_search_result searched = find_good_or_last(block, query);
+  if (searched.type == BSR_FOUND_GOOD_BLOCK){
+      split_if_too_big(searched.block, query);
+      searched.block->is_free = false;
+  }
+  return searched;
 }
 
 
 
 static struct block_header* grow_heap( struct block_header* restrict last, size_t query ) {
-  /*  ??? */
+    if (!last) return NULL;
+    struct region new = alloc_region(block_after(last), query);
+    if (!region_is_invalid(&new)){
+        last->next = new.addr;
+        if (try_merge_with_next(last)) return last;
+        return last->next;
+    }
+    return NULL;
 }
 
 /*  Реализует основную логику malloc и возвращает заголовок выделенного блока */
 static struct block_header* memalloc( size_t query, struct block_header* heap_start) {
-
-  /*  ??? */
-
+    size_t new_query = size_max(BLOCK_MIN_CAPACITY, query);
+    struct block_search_result searched = try_memalloc_existing(new_query,heap_start);
+    switch (searched.type) {
+        default: return NULL;
+        case BSR_FOUND_GOOD_BLOCK: return searched.block;
+        case BSR_CORRUPTED: return NULL;
+        case BSR_REACHED_END_NOT_FOUND:
+            searched.block = grow_heap(searched.block, new_query);
+            searched = try_memalloc_existing(new_query, searched.block);
+            if (searched.block != NULL) searched.block->is_free = false;
+            return searched.block;
+    }
 }
 
 void* _malloc( size_t query ) {
@@ -129,5 +188,8 @@ void _free( void* mem ) {
   if (!mem) return ;
   struct block_header* header = block_get_header( mem );
   header->is_free = true;
-  /*  ??? */
+  bool merged_flag = try_merge_with_next(header);
+  while(merged_flag){
+      merged_flag = try_merge_with_next(header);
+  }
 }
